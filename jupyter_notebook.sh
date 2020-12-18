671ecf9bc03e4db7910af06228b77978e9facd72
thomas-broethaler <thomas.broethaler@gmail.com>
2020-12-18 15:41:51 +0100

    adding scripts for automatically opening certain tools which are executed on certain keyboard shortcuts

---
+++ jupyter_notebook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+exec > $HOME/Programme/my_scripts/output_logs/output.log 2>&1
+
+# make functions available in subshells
+source /home/thomas/Programme/anaconda3/etc/profile.d/conda.sh
+
+# activate conda python environment
+conda activate python
+
+# open jupyter notebook
+jupyter notebook
